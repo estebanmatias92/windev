74c2539e1917a7bd97ecfcece9f828946123d7bf
Matias Esteban <estebanmatias92@gmail.com>
2022-04-11 18:11:34 -0300

    Fixing Paths

--- libraries/windows/config/dotfiles.ps1
+++ libraries/windows/config/dotfiles.ps1
@@ -1,9 +1,12 @@
+# Dotfiles Folder Path
+$DotfilesDir = "$($PSScriptRoot)\.dotfiles"
+
 Write-Host "-----------------------------------------------------"
 Write-Host "--- Downloading PowerShell profile and copying it ---"
 Write-Host "-----------------------------------------------------"
 Write-Host ""
 
-. $PSScriptRoot\.dotfiles\powershell-profile.ps1 
+. $DotfilesDir\powershell-profile.ps1 
 
 Write-Host "PowerShell profile is copied"
 Start-Sleep 1
@@ -15,7 +18,7 @@ Write-Host "--- Downloading Windows Terminal Preview settings and copying them -
 Write-Host "----------------------------------------------------------------------"
 Write-Host ""
 
-. $PSScriptRoot\.dotfiles\windows-terminal-preview-settings.ps1
+. $DotfilesDir\windows-terminal-preview-settings.ps1
 
 Write-Host "Windows Terminal Preview settings are copied"
 Start-Sleep 2
