44984cd99fdbc56e419bee9bfdeeec8ded793471
estebanmatias92 <estebanmatias92@gmail.com>
2022-04-16 04:11:49 -0300

    Fixed linux filesystem path for WSL config folder

--- libraries/wsl/configs.ps1
+++ libraries/wsl/configs.ps1
@@ -1,23 +1,22 @@
-SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
-
-$ConfigDir = "/libraries/wsl/config"
+# Setting up the Configuration directory path from within linux (is getting it from the linux filesystem)
+$ConfigDir = (wsl -d $LinuxDistro.Name --cd $PSScriptRoot bash -c pwd) + "/config"
 
 # Settings for Linux Apps
 [System.Collections.ArrayList]$global:WslConfigs = @(
     @{
         Name = "Zsh Modules"
-        Run = "$($global:wsl) bash -c '`$(SCRIPT_DIR)$($ConfigDir)/shell-modules/zsh.sh'"
+        Run = "$($global:wsl) bash -c '$($ConfigDir)/shell-modules/zsh.sh'"
     },
     @{
         Name = "SSH Keys"
-        Run = "$($global:wsl) bash -c '`$(pwd)$($ConfigDir)/ssh.sh'"
+        Run = "$($global:wsl) bash -c '$($ConfigDir)/ssh.sh'"
     },
     @{
         Name = "Github SSH Credentials"
-        Run = "$($global:wsl) bash -c '`$(pwd)$($ConfigDir)/github-ssh-login.sh'"
+        Run = "$($global:wsl) bash -c '$($ConfigDir)/github-ssh-login.sh'"
     },
     @{
         Name = "Dotfiles"
-        Run = "$($global:wsl) bash -c '`$(pwd)$($ConfigDir)/dotfiles.sh'"
+        Run = "$($global:wsl) bash -c '$($ConfigDir)/dotfiles.sh'"
     }
 )
