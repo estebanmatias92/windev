1bd1cce6d03f030fade4004ed0993ad35f0403ef
estebanmatias92 <estebanmatias92@gmail.com>
2022-04-24 19:58:09 -0300

    Making changes to Terminal Preview Settings and Pwsh Settings

--- libraries/windows/config/.dotfiles/powershell-profile.ps1
+++ libraries/windows/config/.dotfiles/powershell-profile.ps1
@@ -3,6 +3,6 @@
 $StartDir = if ($Env:OneDrive) { $Env:OneDrive } else { $Home }
 $FilePath = "$($StartDir)\Documents\PowerShell\Microsoft.PowerShell_profile.ps1"
 
-((New-Object System.Net.WebClient).DownloadString('https://gist.githubusercontent.com/estebanmatias92/863bc01532c6e9593de234b8f88b6bd6/raw/07657100f68b6be0714d2be5a0ad42593be291b0/powershell.profile.ps1')) > $FilePath
+((New-Object System.Net.WebClient).DownloadString('https://gist.githubusercontent.com/estebanmatias92/863bc01532c6e9593de234b8f88b6bd6/raw/2dc81f8ddfe911e574cc408f3132227737566a1f/Microsoft.PowerShell_profile.ps1')) > $FilePath
 
 $FilePath
--- libraries/windows/config/.dotfiles/windows-terminal-preview-settings.ps1
+++ libraries/windows/config/.dotfiles/windows-terminal-preview-settings.ps1
@@ -2,6 +2,6 @@
 
 $FilePath = "$($Env:LOCALAPPDATA)\Packages\Microsoft.WindowsTerminalPreview_8wekyb3d8bbwe\LocalState\settings.json"
 
-((New-Object System.Net.WebClient).DownloadString('https://gist.githubusercontent.com/estebanmatias92/b1bfbf584ca8e18a70f6d46d89c88c27/raw/2a73780fbc8f17d5cabac7df22f4ba23f3e5eeac/terminal-preview-settings.json')) > $FilePath
+((New-Object System.Net.WebClient).DownloadString('https://gist.githubusercontent.com/estebanmatias92/b1bfbf584ca8e18a70f6d46d89c88c27/raw/313dabca0c251e432d432d1f2eeca550e7277ba1/settings.json')) > $FilePath
 
 $FilePath
