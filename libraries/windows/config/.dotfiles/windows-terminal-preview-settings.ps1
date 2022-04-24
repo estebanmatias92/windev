bee92d530699abbada0616203f0df54a2e594674
estebanmatias92 <estebanmatias92@gmail.com>
2022-04-24 20:59:39 -0300

    Modify Windows Dotfiles source url

--- libraries/windows/config/.dotfiles/windows-terminal-preview-settings.ps1
+++ libraries/windows/config/.dotfiles/windows-terminal-preview-settings.ps1
@@ -2,6 +2,6 @@
 
 $FilePath = "$($Env:LOCALAPPDATA)\Packages\Microsoft.WindowsTerminalPreview_8wekyb3d8bbwe\LocalState\settings.json"
 
-((New-Object System.Net.WebClient).DownloadString('https://gist.githubusercontent.com/estebanmatias92/b1bfbf584ca8e18a70f6d46d89c88c27/raw/313dabca0c251e432d432d1f2eeca550e7277ba1/settings.json')) > $FilePath
+((New-Object System.Net.WebClient).DownloadString('https://gist.githubusercontent.com/estebanmatias92/b1bfbf584ca8e18a70f6d46d89c88c27/raw/54a00a830ad27dad9325cc19604ec64576ea26c4/settings.json')) > $FilePath
 
 $FilePath
